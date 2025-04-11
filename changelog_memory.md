0b0d40562a8dec2942fe99a1d163fecbbb4f6647
FanMinkJonk <127284415+FanMinkJonk@users.noreply.github.com>
2025-04-11 14:31:21 +0700

    - Adjusted read_en/write_en comments - Added data_buffer to store data then assign it to the data - Adjust _memory register declaration - Implement basic functionalities of the memory block

--- changelog_memory.md
+++ changelog_memory.md
@@ -5,3 +5,9 @@
 
 11:08-11/04/2025
 - Minor errors fixed
+
+[1.0.0] 12:52-11/04/2025
+- Adjusted read_en/write_en comments
+- Added data_buffer to store data then assign it to the data 
+- Adjust _memory register declaration
+- Implement basic functionalities of the memory block
--- memory.v
+++ memory.v
@@ -11,16 +11,29 @@
 
 module memory (
         input       clk,      // System clock (posedge triggered)
-        input       read_en,  // Active-high asynchronous read memory enable signal
-        input       write_en, // Active-high asynchronous write memory enable signal
+        input       read_en,  // Active-high asynchronous read from memory enable signal
+        input       write_en, // Active-high asynchronous write to memory enable signal
         input [4:0] address,  // 5-bit memory access address
-        inout [7:0] data      // 8-bit data
+        inout [7:0] data_bus  // 8-bit data
 );
 
     //------------------------------------------
     // Internal Registers
     //------------------------------------------
-    reg [255:0] _memory; // Main storage for datas and instructions
-    
+    reg [7:0] _memory [255:0]; // Main storage for datas and instructions
+    reg [7:0] data_buffer;     // Data buffer for writing data to data bus
+
+    //------------------------------------------
+    // Read/write execution
+    //------------------------------------------
+    assign data_bus = read_en ? data_buffer : 8'bz;
+    always @(posedge clk) begin
+        if (read_en) begin
+            data_buffer = _memory[address];
+        end
+        else if (write_en) begin
+            _memory[address] = data_bus;
+        end
+    end
 
 endmodule
